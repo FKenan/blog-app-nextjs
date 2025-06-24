b1de262150091ea008e80c412c764481c105c696
FKenan <kenanf734@gmail.com>
2025-06-24 17:29:14 +0300

    Sayfaya göre title değiştirme

---
+++ src/app/about/page.js
@@ -0,0 +1,7 @@
+//title ezme. %s kısmına About eklenir.
+export const metadata = {
+  title: "About",
+};
+export default function Page() {
+  return <div>Page</div>;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
 //npx create-next-app@latest ile proje oluşturduk.
 //npm run dev ile projeyi çalıştırırız.
 
+// localhost:3000/ sayfasına denk gelir.
+// app klasörü altındaki page sayfası ana sayfa olarak algılanır.
+// app içine açılan klasörler routing yapısına katılır.
+// blogs klasörü localhost:3000/blogs a denk gelir. blogs altındaki page.js sayfası açılır.
+
 export default function Home() {
   return <h1>Home</h1>;
 }
