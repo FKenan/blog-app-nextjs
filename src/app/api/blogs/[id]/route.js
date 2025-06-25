40f6d6f59aa126b4c96746ee3b4ce2b77e15c124
FKenan <kenanf734@gmail.com>
2025-06-25 15:21:43 +0300

    API => databaseden tüm verileri çekme ve idye göre veri çekme

---
+++ src/app/api/blogs/[id]/route.js
@@ -0,0 +1,15 @@
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+let db = null;
+
+export async function GET(request, { params }) {
+  const { id } = params;
+  if (!db) {
+    db = await open({ filename: "./blog.db", driver: sqlite3.Database });
+  }
+
+  const result = await db.get("SELECT * FROM blogs where id=?", id);
+
+  return Response.json(result);
+}
